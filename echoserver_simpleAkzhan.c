d8970f0f90a394bc06ecfdad648109a21b9b5d2d
Pupye <fullhouseya89@gmail.com>
2020-04-16 21:00:29 +0600

    updated akzhaan

--- echoserver_simpleAkzhan.c
+++ echoserver_simpleAkzhan.c
@@ -7,215 +7,327 @@
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <netinet/in.h>
+#include <sys/select.h>
 #include <pthread.h>
 #include <semaphore.h>
 #include <prodcon.h>
 #include <fcntl.h>
-//#define    QLEN            5
-//#define    BUFSIZE            4096
-int bufferSize;
-//char            buf[bufferSize];
+#include <sys/stat.h>
 
-//int passivesock( char *service, char *protocol, int qlen, int *rport );
+
+#define COMMAND_BUFFER 512
+#define SSOCK_SIZE 1024
 
 sem_t *full;  // initial value is 0
 sem_t *empty; // initial value is buff_size
 sem_t *mutex; // mutex for count
 int indx = 0; // the index that points to the item that can be consumed
+int bufferSize;
 
 int conNumber = 0;
 int proNumber = 0;
 
-//typedef struct Item_struct {
-//    int size;
-//    char * letters;
-//}item_struct;
-
 ITEM *buffItem[BUFSIZE];
 
-void *serve(void *tid)
-{
-	int cc;
-	int *ssock = (int *)tid;
+void *produce(void *fd){
+    
+    int *ssock = (int *)fd;
+    
+    int letterCount;
+    ITEM *chosenItem = malloc(sizeof(ITEM));
+    
+    
+    sem_wait(mutex);
+    if (proNumber > MAX_PROD)
+    {
+        sem_post(mutex);
+        close(*ssock);
+        pthread_exit(0);
+    }
+    else if (proNumber + conNumber > MAX_CLIENTS)
+    {
+        sem_post(mutex);
+        close(*ssock);
+        pthread_exit(0);
+    }
+    else if (buffItem[indx]==NULL){
+        proNumber++;
+    }
+    sem_post(mutex);
+    
+    printf("prod num is %i\n",proNumber);
+    
+    write(*ssock, "GO\r\n", 4);
+    
+    read(*ssock, &letterCount, 4);
+    letterCount = ntohl(letterCount);
+    chosenItem->size = letterCount;
+    printf(" size is %i \n", chosenItem->size); // getting size
+    
+    chosenItem->letters = (char *)malloc(chosenItem->size);
+    int read_index = 0;
+    int cc;
+    while (read_index < chosenItem->size)
+    {
+        cc = read(*ssock, chosenItem->letters + read_index, chosenItem->size - read_index);
+        read_index += cc;
+        if (cc <= 0)
+        {
+            break;
+        }
+    }
+    if (read_index < chosenItem->size)
+    {
+        printf("Unexpected behavior\n");
+        fflush(stdout);
+        close(*ssock);
+        free(chosenItem->letters);
+        free(chosenItem);
+        pthread_exit(0);
+    }
 
-	/* start working for this guy */
-	/* ECHO what the client says */
-	char buf[512];
-
-	if ((cc = read(*ssock, buf, bufferSize)) <= 0)
-	{
-
-		printf("The client %i has gone.\n", *ssock);
-		close(*ssock);
-	}
-	else
-	{
-		buf[cc] = '\0';
-		printf("%s \n", buf);
-		if (strncmp(buf, "PRODUCE\r\n", 9) == 0)
-		{
-			int letterCount;
-			ITEM *chosenItem = malloc(sizeof(ITEM));
-			sem_wait(empty);
-			sem_wait(mutex);
-			if (proNumber > MAX_PROD)
-			{
-				pthread_exit(0);
-			}
-			else if (proNumber + conNumber > MAX_CLIENTS)
-			{
-				close(*ssock);
-				pthread_exit(0);
-			}
-			else
-			{
-				proNumber++;
-			}
-			sem_post(mutex);
-
-			write(*ssock, "GO\r\n", 4);
-			//choose the empty space in the buffer
-			read(*ssock, &letterCount, 4);
-			letterCount = ntohl(letterCount);
-			//create space with such size
-			chosenItem->size = letterCount;
-			printf(" size is %i \n", chosenItem->size);
-			chosenItem->letters = (char *)malloc(chosenItem->size + 1);
-			read(*ssock, chosenItem->letters, chosenItem->size);
-			printf("read this %s \n", chosenItem->letters);
-			write(*ssock, "DONE\r\n", 7);
-			close(*ssock);
-
-			sem_wait(mutex);
-			buffItem[indx] = chosenItem;
-			indx++;
-			proNumber--;
-			sem_post(mutex);
-			sem_post(full);
-		}
-		if (strncmp(buf, "CONSUME\r\n", 9) == 0)
-		{
-			int letterCount;
-			ITEM *chosenItem;
-			sem_wait(full);
-			sem_wait(mutex);
-			if (conNumber > MAX_CON)
-			{
-				sem_post(mutex);
-				pthread_exit(0);
-			}
-			else if ((proNumber + conNumber) > MAX_CLIENTS)
-			{
-				sem_post(mutex);
-				close(*ssock);
-				pthread_exit(0);
-			}
-			else
-			{
-				conNumber++;
-			}
-			indx--;
-			chosenItem = buffItem[indx];
-			buffItem[indx] = NULL;
-			sem_post(mutex);
-			printf("%i\n", chosenItem->size);
-			letterCount = htonl(chosenItem->size);
-			write(*ssock, &letterCount, 4);
-			write(*ssock, chosenItem->letters, chosenItem->size);
-			close(*ssock);
-			chosenItem->size = 0;
-			free(chosenItem->letters);
-			sem_wait(mutex);
-			conNumber--;
-			sem_post(mutex);
-			sem_post(empty);
-		}
-		printf("The client %i says: %s\n", *ssock, buf);
-		if (write(*ssock, buf, cc) < 0)
-		{
-			/* This guy is dead */
-			close(*ssock);
-		}
-	}
-	pthread_exit(0);
+    
+    sem_wait(empty);
+    sem_wait(mutex);
+    buffItem[indx] = chosenItem;
+    indx++;
+    proNumber--;
+    sem_post(mutex);
+    sem_post(full);
+    
+    write(*ssock, "DONE\r\n", 6);
+    
+    close(*ssock);
+    pthread_exit(0);
 }
+void *consume(void *fd){
+    int *ssock = (int *)fd;
+    
+    sem_wait(mutex);
+    if (conNumber > MAX_CON)
+    {
+        sem_post(mutex);
+        close(*ssock);
+        pthread_exit(0);
+    }
+    else if ((proNumber + conNumber) > MAX_CLIENTS)
+    {
+        sem_post(mutex);
+        close(*ssock);
+        pthread_exit(0);
+    }
+    else if (buffItem[indx]!=NULL)
+    {
+        conNumber++;
+    }
+    sem_post(mutex);
+    
+    int letterCount;
+    ITEM *chosenItem;
+    
+    sem_wait(full);
+    sem_wait(mutex);
+    indx--;
+    chosenItem = buffItem[indx];
+    buffItem[indx] = NULL;
+    sem_post(mutex);
+    sem_post(empty);
+    letterCount = htonl(chosenItem->size);
+    write(*ssock, &letterCount, 4);
+    write(*ssock, chosenItem->letters, chosenItem->size);
+    chosenItem->size = 0;
+    free(chosenItem->letters);
+    free(chosenItem);
+    sem_wait(mutex);
+    conNumber--;
+    sem_post(mutex);
+    
+    close(*ssock);
+    pthread_exit(0);
+}
+
 /*
-**	This poor server ... only serves one client at a time
-*/
-int main(int argc, char *argv[])
+ **    The server ... uses multiplexng to switch between clients
+ **    Each client gets one echo per turn,
+ **    but can have as many echoes as it wants until it disconnects
+ */
+int
+main( int argc, char *argv[] )
 {
-
-	char *service;
-	struct sockaddr_in fsin;
-	socklen_t alen;
-	int msock;
-	int ssock;
-	int rport = 0;
-	int cc;
-	int i = 0;
-
-	switch (argc)
-	{
-	case 1:
-		// No args? let the OS choose a port and tell the user
-		rport = 1;
-		break;
-	case 2:
-		// User provides a port? then use it
-		service = argv[1];
-		exit(-1);
-		break;
-	case 3:
-		service = argv[1];
-		bufferSize = atoi(argv[2]);
-
-		if (bufferSize > BUFSIZE)
-		{
-			printf("too long buffer size");
-			exit(-1);
-		}
-		break;
-	default:
-		fprintf(stderr, "usage: server [port]\n");
-		exit(-1);
-	}
-	printf("%i \n", bufferSize);
-	msock = passivesock(service, "tcp", QLEN, &rport);
-	if (rport)
-	{
-		//	Tell the user the selected port
-		printf("server: port %d\n", rport);
-		fflush(stdout);
-	}
-
-	sem_unlink("full");
-	sem_unlink("empty");
-	sem_unlink("mutex");
-	//initialization of semaphores and global vars
-	full = sem_open("full", O_CREAT | O_EXCL, S_IRWXU, 0);
-	empty = sem_open("empty", O_CREAT | O_EXCL, S_IRWXU, bufferSize);
-	mutex = sem_open("mutex", O_CREAT | O_EXCL, S_IRWXU, 1);
-
-	//
-	for (;;)
-	{
-		int ssock[1024];
-
-		alen = sizeof(fsin);
-		ssock[i] = accept(msock, (struct sockaddr *)&fsin, &alen);
-		if (ssock < 0)
-		{
-			fprintf(stderr, "accept: %s\n", strerror(errno));
-			exit(-1);
-		}
-
-		printf("A client has arrived for echoes.\n");
-		fflush(stdout);
-
-		pthread_t tid;
-		pthread_create(&tid, NULL, serve, &ssock[i]);
-		i++;
-		fflush(stdout);
-	}
-	pthread_exit(0);
+    char            buf[BUFSIZE];
+    char            *service;
+    struct sockaddr_in    fsin;
+    int            msock;
+    int            ssock;
+    fd_set            rfds;
+    fd_set            afds;
+    int            alen;
+    int            fd;
+    int            nfds;
+    int            rport = 0;
+    int            cc;
+    
+    // Same arguments as usual
+    switch (argc)
+    {
+        case    1:
+            // No args? let the OS choose a port and tell the user
+            rport = 1;
+            break;
+        case    2:
+            // User provides a port? then use it
+            service = argv[1];
+            break;
+        case    3:
+            service = argv[1];
+            bufferSize = atoi(argv[2]);
+            
+            if (bufferSize > BUFSIZE)
+            {
+                printf("too long buffer size");
+                exit(-1);
+            }
+            break;
+        default:
+            fprintf( stderr, "usage: server [port]\n" );
+            exit(-1);
+    }
+    
+    // Create the main socket as usual
+    // This is the socket for accepting new clients
+    msock = passivesock( service, "tcp", QLEN, &rport );
+    if (rport)
+    {
+        //    Tell the user the selected port
+        printf( "server: port %d\n", rport );
+        fflush( stdout );
+    }
+    
+    sem_unlink("full");
+    sem_unlink("empty");
+    sem_unlink("mutex");
+    //initialization of semaphores and global vars
+    full = sem_open("full", O_CREAT | O_EXCL, S_IRWXU, 0);
+    empty = sem_open("empty", O_CREAT | O_EXCL, S_IRWXU, bufferSize);
+    mutex = sem_open("mutex", O_CREAT | O_EXCL, S_IRWXU, 1);
+    
+    // Now we begin the set up for using select
+    
+    // nfds is the largest monitored socket + 1
+    // there is only one socket, msock, so nfds is msock +1
+    // Set the max file descriptor being monitored
+    nfds = msock+1;
+    
+    // the variable afds is the fd_set of sockets that we want monitored for
+    // a read activity
+    // We initialize it to empty
+    FD_ZERO(&afds);
+    
+    // Then we put msock in the set
+    FD_SET( msock, &afds );
+    
+    // Now start the loop
+    for (;;)
+    {
+        // Since select overwrites the fd_set that we send it,
+        // we copy afds into another variable, rfds
+        // Reset the file descriptors you are interested in
+        memcpy((char *)&rfds, (char *)&afds, sizeof(rfds));
+        
+        // Only waiting for sockets who are ready to read
+        //  - this includes new clients arriving
+        //  - this also includes the client closed the socket event
+        // We pass null for the write event and exception event fd_sets
+        // we pass null for the timer, because we don't want to wake early
+        if (select(nfds, &rfds, (fd_set *)0, (fd_set *)0,
+                   (struct timeval *)0) < 0)
+        {
+            fprintf( stderr, "server select: %s\n", strerror(errno) );
+            exit(-1);
+        }
+        
+        // Since we've reached here it means one or more of our sockets has something
+        // that is ready to read
+        // So now we have to check all the sockets in the rfds set which select uses
+        // to return a;; the sockets that are ready
+        
+        // If the main socket is ready - it means a new client has arrived
+        // It must be checked separately, because the action is different
+        if (FD_ISSET( msock, &rfds))
+        {
+            int    ssock;
+            
+            // we can call accept with no fear of blocking
+            alen = sizeof(fsin);
+            ssock = accept( msock, (struct sockaddr *)&fsin, &alen );
+            if (ssock < 0)
+            {
+                fprintf( stderr, "accept: %s\n", strerror(errno) );
+                exit(-1);
+            }
+            
+            // If a new client arrives, we must add it to our afds set
+            FD_SET( ssock, &afds );
+            
+            // and increase the maximum, if necessary
+            if ( ssock+1 > nfds )
+                nfds = ssock+1;
+            
+           
+        }
+        
+        // Now check all the regular sockets
+        for ( fd = 0; fd < nfds; fd++ )
+        {
+            // check every socket to see if it's in the ready set
+            // But don't recheck the main socket
+            if (fd != msock && FD_ISSET(fd, &rfds))
+            {
+                // you can read without blocking because data is there
+                // the OS has confirmed this
+                if ( (cc = read( fd, buf, BUFSIZE )) <= 0 )
+                {
+                    printf( "The client has gone.\n" );
+                    (void) close(fd);
+                    
+                    // If the client has closed the connection, we need to
+                    // stop monitoring the socket (remove from afds)
+                    FD_CLR( fd, &afds );
+                   
+                    // lower the max socket number if needed
+                    if ( nfds == fd+1 )
+                        nfds--;
+                    
+                }
+                else
+                {
+                    
+                    // Otherwise send the echo to the client
+                    buf[cc]='\0';
+                    pthread_t tid;
+                    if(strncmp(buf,"PRODUCE\r\n",9)==0){
+                        
+                        pthread_create(&tid, NULL, produce, &fd);
+                    }
+                    else if (strncmp(buf, "CONSUME\r\n", 9) == 0){
+                        
+                        pthread_create(&tid, NULL, consume, &fd);
+                    }
+                    
+                    
+                    
+                }
+                
+                (void) close(fd);
+                FD_CLR( fd, &afds );
+                
+                
+                
+                
+            }
+            
+        }
+        
+    }
+    pthread_exit(0);
 }
